dda65207de67800dfa1a15cdbcbb28641fa0b065
gTaitasaur <gTaitasaur@gmail.com>
2023-11-18 05:40:18 -0300

    Create UserModel.java

---
+++ Ev3_AppMoviles/app/src/main/java/com/example/ev2_app/UserModel.java
@@ -0,0 +1,42 @@
+package com.example.ev2_app;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable{
+
+    private String name, hobby, favnumber;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getFavnumber() {
+        return favnumber;
+    }
+
+    public void setFavnumber(String favnumber) {
+        this.favnumber = favnumber;
+    }
+
+}
