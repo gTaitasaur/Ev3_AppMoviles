854e178adf1faa3baa71acecb2fca2879c53b441
gTaitasaur <gTaitasaur@gmail.com>
2023-11-18 01:13:01 -0300

    Ir y volver

--- Ev3_AppMoviles/app/src/main/java/com/example/ev2_app/MuestaDatos.java
+++ Ev3_AppMoviles/app/src/main/java/com/example/ev2_app/MuestaDatos.java
@@ -2,7 +2,10 @@ package com.example.ev2_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MuestaDatos extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MuestaDatos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muesta_datos);
+
+        //      CAMBIA PANTALLA A MOSTRAR DATOS
+        Button buttonVolver = findViewById(R.id.button_volver);
+        buttonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MuestaDatos.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
