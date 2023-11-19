907abe65ad670280e3d873379d380807cf160d89
Diobreque <117220996+Diobreque@users.noreply.github.com>
2023-11-18 21:56:14 -0300

    VAMOOOO CHILEEEEEEEEEEEEEEEEEEEEEEEE
    SACHEIIIIIII

--- Ev3_AppMoviles/app/src/main/java/com/example/ev2_app/UpdateDelete.java
+++ Ev3_AppMoviles/app/src/main/java/com/example/ev2_app/UpdateDelete.java
@@ -2,17 +2,11 @@ package com.example.ev2_app;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
 
 public class UpdateDelete extends AppCompatActivity {
 
@@ -21,52 +15,56 @@ public class UpdateDelete extends AppCompatActivity {
     private Button actualizar, eliminar;
     private Sqlito databaseHelper;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_update_delete); // Asegúrate de que este es el layout correcto
 
         Intent intent = getIntent();
         userModel = (UserModel) intent.getSerializableExtra("user");
 
         databaseHelper = new Sqlito(this);
 
-        etFecha = (EditText) findViewById(R.id.editTextFecha);
-        etHora = (EditText) findViewById(R.id.editTextHora);
-        etNombre = (EditText) findViewById(R.id.text_nombre);
-        etRut = (EditText) findViewById(R.id.text_rut);
-        etDescripcion = (EditText) findViewById(R.id.text_descripcion);
-        etLab = (EditText) findViewById(R.id.text_nombre);
+        etFecha = findViewById(R.id.etFecha);
+        etHora = findViewById(R.id.etHora);
+        etNombre = findViewById(R.id.etNombre);
+        etRut = findViewById(R.id.etRut);
+        etDescripcion = findViewById(R.id.etDescripcion);
+        etLab = findViewById(R.id.etLab);
 
-        etFecha.setText(userModel.getFecha());
-        etNombre.setText(userModel.getNombre());
-        etRut.setText(userModel.getRut());
-        etDescripcion.setText(userModel.getDescription());
-        etLab.setText(userModel.getLaboratorio());
+        actualizar = findViewById(R.id.btnActualizar);
+        eliminar = findViewById(R.id.btnEliminar);
+
+        // Establecer los valores actuales en los EditTexts
+        if (userModel != null) {
+            etFecha.setText(userModel.getFecha());
+            etNombre.setText(userModel.getNombre());
+            etRut.setText(userModel.getRut());
+            etDescripcion.setText(userModel.getDescription());
+            etLab.setText(userModel.getLaboratorio());
+        }
 
         actualizar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                databaseHelper.updateUser(userModel.getId(),etFecha.getText().toString(),etHora.getText().toString(),etNombre.getText().toString(), etRut.getText().toString(),etLab.getText().toString(), etDescripcion.getText().toString());
+                databaseHelper.updateUser(userModel.getId(),
+                        etFecha.getText().toString(),
+                        etHora.getText().toString(),
+                        etNombre.getText().toString(),
+                        etRut.getText().toString(),
+                        etLab.getText().toString(),
+                        etDescripcion.getText().toString());
                 Toast.makeText(UpdateDelete.this, "Actualizado!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(UpdateDelete.this, MuestaDatos.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                finish(); // Cierra la actividad actual y vuelve a la anterior
             }
         });
-
 
         eliminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 databaseHelper.deleteUSer(userModel.getId());
-                Toast.makeText(UpdateDelete.this, "ELIMINADO!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(UpdateDelete.this, MuestaDatos.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                Toast.makeText(UpdateDelete.this, "Eliminado!", Toast.LENGTH_SHORT).show();
+                finish(); // Cierra la actividad actual y vuelve a la anterior
             }
         });
-
-
-
     }
-
 }
